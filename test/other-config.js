b84c225d250a73bd9983f45d9b8b5c40c9425463
Jerry Hamlet <jerry@hamletink.com>
2011-11-07 09:49:25 -0800

    Update Stitchfile with more samples
    Update regex for getting "[config:]bundle.type" from command arg
    
    Add "id" properties to all models.
    
    Correctly call BundleModel's super.get method.

--- test/other-config.js
+++ test/other-config.js
@@ -1,11 +1,4 @@
 
-console.log(env["HELLO_WORLD"]);
-console.log(typeof env["HELLO_WORLD"]);
-
-console.log("Options:");
-console.log(options.minify);
-
 configure(function () {
-    console.log("other config");
     this.sourcePaths.push("other-source-path");
 });
