ec7b0bff95f9d329995cc22d1428d42c6a99d389
Jerry Hamlet <jerry@hamletink.com>
2011-11-12 21:48:00 -0800

    Big update to re-expose the current driver's methods and properties to the global scope which help improves the DSL "feel".

--- test/sample.js
+++ test/sample.js
@@ -6,58 +6,62 @@ var should      = require("should"),
 
 stitch.run(function (stitch) {
 
-    stitch.define_type("text/stylesheet", "scss");
-    stitch.define_type("text/stylesheet", "less");
+    define_type("text/stylesheet", "scss");
+    define_type("text/stylesheet", "less");
 
-    stitch.type("text/javascript").extension = "jss";
+    type("text/javascript").extension = "jss";
     
     // This file is usually run in the main directory so we have to prefix
     // the directory to get to the file.
-    stitch.include("test/other-config.js");
+    include("test/other-config.js");
     
-    stitch.configure(function (cfg) {
-        cfg.source_paths.push('path-to-source-directory');
+    configure(function (cfg) {
+        source_paths.push('path-to-source-directory');
 
-        cfg.desc = "The default configuration.";
+        desc = "The default configuration.";
 
-        cfg.bundle('core', function (core) {
-            core.desc = "The core module.";
+        bundle('core', function (core) {
+            desc = "The core module.";
 
-            core.insert("--core module comment--");
+            insert("--core module comment--");
 
-            core.javascript(function () {
-                core.add('path-to-file.js');
-                core.add('path-to-other-file.js');
+            javascript(function () {
+                add('path-to-file.js');
+                add('path-to-other-file.js');
             });
 
-            core.scss(function () {
-                core.include("path-to-core.scss");
+            scss(function () {
+                file("path-to-core.scss");
             });
             
         });
-    }).
-    bundle('sub', function () {
-        this.desc = "A submodule description.";
-        this.desc = "that goes on and on";
 
-        // include another module's definitions
-        this.require('core');
+        bundle('sub', function () {
+            console.log(insert.toString());
+            
+            desc = "A submodule description.";
+            desc = "that goes on and on";
 
-        // JavaScript dependencies
-        this.include('sub-path-to-file.js');
-        this.include("js", 'sub-path-to-other-file.js'); // say what type of asset it is
+            // include another module's definitions
+            include('core');
 
-        this.fetch("js", 'http://uri-to-content-to-include');
+            // JavaScript dependencies
+            file('sub-path-to-file.js');
+            file("js", 'sub-path-to-other-file.js'); // say what type of asset it is
 
-        // Add comments: these will be prefixed with the '/*!' style so most/some
-        // minifiers will leave these comments intact.
-        this.insert('Include a direct comment into\nthe generated output.');
-        this.include('path-to-comment-file.txt');
+            fetch("js", 'http://uri-to-content-to-include');
 
-        // CSS dependencies
-        this.include("css", 'sub-path-to-file.scss');
-        this.include("css", 'sub-path-to-other-file.scss');
+            // Add comments: these will be prefixed with the '/*!' style so most/some
+            // minifiers will leave these comments intact.
+            insert('Include a direct comment into\nthe generated output.');
+            file('path-to-comment-file.txt');
+
+            // CSS dependencies
+            file("css", 'sub-path-to-file.scss');
+            file("css", 'sub-path-to-other-file.scss');
+        });
     });
+    
 });
 
 // stitch.file("./other-config.js");
