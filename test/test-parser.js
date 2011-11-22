1f19235325fb5497bb15bb200c9912a120751656
Jerry Hamlet <jerry@hamletink.com>
2011-11-21 20:58:46 -0800

    Updated parser.
    Got rid of extra classes and support modules.

--- test/test-parser.js
+++ test/test-parser.js
@@ -5,14 +5,21 @@ var Parser  = require("../lib/parser"),
 module.exports = {
     "Base": function () {
         var test = new Parser({
-            directives: {
-                "include":  ["include", function (parser) {}],
-                "@depends?": ["depend", function (parser) {}],
-                "@INCLUDE":  ["include", function (parser) {}]
-            }
+            directives: [
+                ["include", /include/, function () {
+                    console.log(this.preDirectiveMatch);
+                    return true;
+                }],
+                ["depend", /@/, function () { return true; } ],
+                ["include", /@/, function () { return true; } ]
+            ]
         });
         
-        test.parse(FS.readFileSync("Stitchfile"));
+        test.parse(FS.readFileSync("Stitchfile"), {
+            include: function () {
+                console.log(arguments);
+            }
+        });
         
         console.log(test);
     }
