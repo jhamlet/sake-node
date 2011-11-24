35ce79aaee6b965938352f80fce04c7476b4f1e0
Jerry Hamlet <jerry@hamletink.com>
2011-11-24 12:47:03 -0800

    FileList almost complete. Looks like an array with some slight differences.
    Use #count instead of #length, and use parentheses '()' to access indexes, and '(idx, val)' to set.

---
+++ test/test-file-list.js
@@ -0,0 +1,55 @@
+
+var should = require("should"),
+    FileList = require("../lib/file-list")
+;
+
+module.exports = {
+    "Exists": function () {
+
+        var fl = new FileList();
+        
+        should.exist(fl);
+        fl.should.be.a("function");
+    },
+    
+    "Include Glob": function () {
+        var fl = new FileList();
+        
+        fl.include("test/test-*");
+        
+        fl.items.should.contain("test/test-file-list.js");
+    },
+    
+    "Exclude Glob": function () {
+        var fl = new FileList();
+        
+        fl.include("test/*.js");
+        fl.exclude("test/test-*");
+        
+        fl.items.should.contain("test/other-config.js");
+        fl.items.should.contain("test/sample.js");
+    },
+    
+    "Exclude RegExp": function () {
+        var fl = new FileList();
+        
+        fl.include("test/*.js");
+        fl.exclude(/test\/(?!test).*\.js$/);
+        
+        fl.items.should.not.contain("test/other-config.js");
+        fl.items.should.not.contain("test/sample.js");
+    },
+    
+    "Returned items is not internal items": function () {
+        var fl = new FileList(),
+            ret;
+
+        fl.include("test/*.js");
+        
+        ret = fl.sort(function (a, b) {
+            return a < b;
+        });
+
+        ret.should.not.eql(fl.__items__);
+    }
+};
