c990806d37a6712305eede1622d0b35b8b7490c8
Jerry Hamlet <jerry@hamletink.com>
2011-12-25 10:51:34 +0100

    Update util#aliasMethod to emit an error if the object or method does not exist. Update test-util with more in-depth #splice testing.

--- test/test-util.js
+++ test/test-util.js
@@ -8,7 +8,26 @@ module.exports = {
         var list = [1, 2, 3, 4, 5];
         
         util.splice(list, 0, list.length);
-        
         list.length.should.equal(0);
+        
+        list = [1, 2, 3];
+        util.splice(list, [4, 5]);
+        list.length.should.equal(5);
+        list[3].should.equal(4);
+        
+        list = [1, 2, 5];
+        util.splice(list, 2, 0, [3, 4]);
+        list.length.should.equal(5);
+        list[2].should.equal(3);
+        
+        list = [1, 2, 5];
+        util.splice(list, 2, 0, 3, 4);
+        list.length.should.equal(5);
+        list[2].should.equal(3);
+
+        list = [1, 2, 5];
+        util.splice(list, 2, [3, 4]);
+        list.length.should.equal(5);
+        list[2].should.equal(3);
     }
 }
